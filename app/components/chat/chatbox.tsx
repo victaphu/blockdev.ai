07f3f8ddeffd8f7802e51f347dcb0a3036e8ebf2
Victa Phu <zain@mysolat.co>
2024-03-02 08:38:42 +1100

    update: text colour in user textbox is white

--- app/components/chat/chatbox.tsx
+++ app/components/chat/chatbox.tsx
@@ -14,7 +14,7 @@ type ChatboxProps = {
 export default function Chatbox(props: ChatboxProps) {
   const { user, notice, instruction, chat } = props;
 
-  return <div className={"p-5 rounded-xl w-full max-w-5xl " + (props.user ? 'bg-white' : 'bg-secondary-content text-primary-content')}>
+  return <div className={"p-5 rounded-xl w-full max-w-5xl " + (props.user ? 'bg-white text-black' : 'bg-secondary-content text-primary-content')}>
     <div className="flex flex-col gap-2">
       <div className="flex flex-row gap-2 items-start">
         <div className={"my-1 w-4 h-4 rounded-full border-white border "  + (props.user ? 'bg-primary-content' : 'bg-secondary')}></div>
